fa5797374c4ecfc9f95982b2c88448a64f536dce
LightningNemesis <ak.kumar220919@gmail.com>
2023-12-24 17:14:31 +0530

    Added JSON server + JSON database, defined CRUD API calls

--- books/src/App.js
+++ books/src/App.js
@@ -1,23 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  // Get all books stored in DB
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data)
+  }
+
+  // Editing books: needs an id
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
-      return book;
+      return book
     });
 
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  // Deleting books
+  const deleteBookById = async(id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+    
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -25,15 +44,19 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
+  // Creating books
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    });
+
     const updatedBooks = [
       ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
+      response.data
     ];
     setBooks(updatedBooks);
+
+
   };
 
   return (
